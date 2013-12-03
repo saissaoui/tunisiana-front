83d479ed47a45a55bc27bf1542e81581379793b0
stef <stef@ubuntu.(none)>
2013-12-03 05:25:09 +0100

    03/12

---
+++ js/header.js
@@ -0,0 +1,15 @@
+$(function() {
+		
+		
+		//menu script
+		$( "#menu" ).menu();
+		// Hover states on the static widgets
+		$( "#dialog-link, #icons li" ).hover(
+			function() {
+				$( this ).addClass( "ui-state-hover" );
+			},
+			function() {
+				$( this ).removeClass( "ui-state-hover" );
+			}
+		);
+	});
