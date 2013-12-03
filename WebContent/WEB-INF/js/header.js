4da53eb367253df15e6292f8f48f9ced3a194b0e
saissaoui <sofiene.aissaoui@gmail.com>
2013-12-03 18:15:08 +0100

    stef

---
+++ WebContent/WEB-INF/js/header.js
@@ -0,0 +1,17 @@
+$(function() {
+		
+		
+	
+
+		//menu script
+		$( "#menu" ).menu();
+		// Hover states on the static widgets
+		$( "#dialog-link, #icons li" ).hover(
+			function() {
+				$( this ).addClass( "ui-state-hover" );
+			},
+			function() {
+				$( this ).removeClass( "ui-state-hover" );
+			}
+		);
+	});
